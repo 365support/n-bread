f17af79a68bba7cd9acefc3843efc2b6ed9da84b
365support <mgjiwon@gmail.com>
2023-04-11 18:58:23 +0900

    chore: 스토리북 nodemodules 사용하지 않도록 설정

---
+++ .storybook/main.js
@@ -0,0 +1,24 @@
+const config = {
+  stories: ["../src/**/*.mdx", "../src/**/*.stories.@(js|jsx|ts|tsx)"],
+  addons: [
+    "@storybook/addon-links",
+    "@storybook/addon-essentials",
+    "@storybook/addon-interactions",
+    "@storybook/addon-mdx-gfm",
+  ],
+  framework: {
+    name: "@storybook/nextjs",
+    options: {},
+  },
+  docs: {
+    autodocs: "tag",
+  },
+  webpackFinal: async (config) => {
+    config.module.rules.push({
+      test: /\.(ts|tsx)$/,
+      loader: require.resolve("babel-loader"),
+    });
+    return config;
+  },
+};
+export default config;
